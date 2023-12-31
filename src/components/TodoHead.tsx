2aea99288ae19b2b5dd8d767c65e9939a61bdab0
sophie925 <sopi920@naver.com>
2023-12-31 23:05:43 +0900

    TodoHead 날짜 보여지는 부분에 현재 날짜 보여지도록 변경

--- src/components/TodoHead.tsx
+++ src/components/TodoHead.tsx
@@ -25,10 +25,17 @@ const TodoHeadBlock = styled.div`
 `;
 
 export default function TodoHead() {
+    const today = new Date();
+    const dateString = today.toLocaleDateString('ko-KR', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+    const dayName = today.toLocaleDateString('ko-KR', { weekday: 'long' });
     return(
         <TodoHeadBlock>
-            <h1>2023년 12월 30일</h1>
-            <div className="day">토요일</div>
+            <h1>{dateString}</h1>
+            <div className="day">{dayName}</div>
             <div className="tasks-left">할 일 2개 남음</div>
         </TodoHeadBlock>
     );
